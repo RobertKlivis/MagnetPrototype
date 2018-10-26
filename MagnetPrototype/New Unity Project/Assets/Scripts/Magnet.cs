820f688e93ce5fcb5e6ee873020de0d38bfb6c56
RobertKlivis <roberts.klivis@gmail.com>
2018-10-26 16:08:57 +0100

    Prototype using magnets
    Added magnets using point effector. Three different car sizes and masses in menu to choose from.

---
+++ MagnetPrototype/New Unity Project/Assets/Scripts/Magnet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour {
+
+    private PointEffector2D pointEffector;
+
+    // Use this for initialization
+    void Start () {
+
+        pointEffector = GetComponent<PointEffector2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            pointEffector.enabled = true;
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            pointEffector.enabled = false;
+        }
+		
+	}
+}
