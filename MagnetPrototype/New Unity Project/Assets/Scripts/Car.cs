b147a99ab4caa86ba912c8d86f3668e44b8c38ab
cbevan200 <CBEVAN200@caledonian.ac.uk>
2018-10-30 01:02:58 +0000

    Car Acceleration update
    A few tweaks to the code appears to show some basic acceleration and deceleration and reverse.

--- MagnetPrototype/New Unity Project/Assets/Scripts/Car.cs
+++ MagnetPrototype/New Unity Project/Assets/Scripts/Car.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class Car : MonoBehaviour {
 
-    public float Velocity;
-
+    float Velocity;
+ 
     // Use this for initialization
     void Start()
     {
-
+        Velocity = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        float currentSpeed = Velocity += Time.deltaTime;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.left), Velocity * Time.deltaTime);
+            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.left), currentSpeed /= (Time.deltaTime));
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.right), Velocity * Time.deltaTime);
+            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.right), currentSpeed *= Velocity * Time.deltaTime);
         }
-
     }
 }
