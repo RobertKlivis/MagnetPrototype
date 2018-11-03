ef4fca01897af580df7b90de8cde31647cb5e21d
RobertKlivis <roberts.klivis@gmail.com>
2018-11-03 15:02:35 +0000

    Added basic acceleration

--- MagnetPrototype/New Unity Project/Assets/Scripts/Car.cs
+++ MagnetPrototype/New Unity Project/Assets/Scripts/Car.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class Car : MonoBehaviour {
 
     float Velocity;
- 
+
+    public float acceleration;
+    public float deceleration;
+    public float maxSpeed;
+    public float maxreverseSpeed;
+
+    private float currentSpeed = 0.0f;
+    private float reverseSpeed = 0.0f;
+
     // Use this for initialization
     void Start()
     {
@@ -15,15 +23,37 @@ public class Car : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        float currentSpeed = Velocity += Time.deltaTime;
+       // float currentSpeed = Velocity += Time.deltaTime;
+
+        currentSpeed += acceleration * Time.deltaTime;
+        reverseSpeed += deceleration * Time.deltaTime;
+
+        if (currentSpeed > maxSpeed)
+        {
+            currentSpeed = maxSpeed;
+        }
+
+        if (reverseSpeed > maxreverseSpeed)
+        {
+            reverseSpeed = maxreverseSpeed;
+        }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.left), currentSpeed /= (Time.deltaTime));
+
+            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.left), reverseSpeed * Time.deltaTime);
+            currentSpeed = 0;
+
+            // transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.left), currentSpeed /= (Time.deltaTime));
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.right), currentSpeed *= Velocity * Time.deltaTime);
+
+            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.right), currentSpeed * Time.deltaTime);
+            reverseSpeed = 0;
+
+            // transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.right), currentSpeed *= Velocity * Time.deltaTime);
         }
     }
 }
