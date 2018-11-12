4a34a6e650009926be4d1cace907fa3d2b9cfb2e
cbevan200 <CBEVAN200@caledonian.ac.uk>
2018-11-12 15:01:31 +0000

    Bridge mechanics provisional update
    mechanics are not yet working, pushing this as unity editor is acting up so no progress is lost.

--- MagnetPrototype/New Unity Project/Assets/Scripts/BridgeScript.cs
+++ MagnetPrototype/New Unity Project/Assets/Scripts/BridgeScript.cs
@@ -1,29 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BridgeScript : MonoBehaviour {
 
-    public float Tilt;
+    public GameObject bridge;
+
+    private Vector3 newPos;
 
 	// Use this for initialization
 	void Start () {
-		
+      
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
+
+    void ChangingPosition()
+    {
+        // Positions that the bridge will interpolate to. 
+        Vector3 A = bridge.transform.position;
+        Vector3 bridgeLandingPosition = new Vector3 (47, 0.3f, 0); // Final position when the bridge is knocked down by the car
+        Quaternion bridgeLandingRotation = new Quaternion(0, 0, 0, 0);
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.transform.tag == "Car")
+        if (other.transform.tag == "Bridge")
         {
-            Tilt += Time.deltaTime * -10;
-            gameObject.transform.rotation = Quaternion.Euler(0, 0, Tilt);
-            gameObject.transform.position = new Vector3(48.0f, -0.55f);
+            ChangingPosition();
+            /* gameObject.transform.rotation = Quaternion.Euler(0, 0, Tilt);
+             gameObject.transform.position = new Vector3(48.0f, -0.55f);
+             */ // These lines of code are automatically translating the bridge
         }
     }
 
 }
+
+//Link to lerp tutorial referenced in this script: https://www.youtube.com/watch?v=cD-mXwSCvWc
